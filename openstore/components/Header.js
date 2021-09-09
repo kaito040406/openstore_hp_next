2aa8a0464e04a1f3968ea5d54ef92cfa5a3adb87
ookuraseiya <seiya.ookura@gmail.com>
2021-09-09 16:11:06 +0900

    修正

--- openstore/components/Header.js
+++ openstore/components/Header.js
@@ -1,11 +1,15 @@
 import Link from 'next/link';
+import Button from '@material-ui/core/Button';
 
 export default function Header() {
   return (
     <>
-      <section header>
+      <section className="header">
         <div className="header-nav">
           <ul className="header-nav-list">
+            <Link href="/posts/first-post">
+              <img className="header-logo" src="./image/openstore-logo.png" />
+            </Link>
             <Link
               href="/posts/first-post"
               className="header-nav-item font-hover"
@@ -36,11 +40,14 @@ export default function Header() {
             >
               ACCESE
             </Link>
-            <Link
-              href="/posts/first-post"
-              className="header-nav-item font-hover"
-            >
-              CONTACT
+            <Link href="/posts/first-post">
+              <Button
+                variant="contained"
+                color="primary"
+                className="header-btn"
+              >
+                お問い合わせ
+              </Button>
             </Link>
           </ul>
         </div>

855a4c29e1aa2f383314d9b8c5a901e60002f8a3
ookuraseiya <seiya.ookura@gmail.com>
2021-09-09 14:55:46 +0900

    いろいろ追加

--- openstore/pages/index.js
+++ openstore/pages/index.js
@@ -1,14 +1,24 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import Header from '../components/Header';
+import Layout from '../components/Common-footer';
 
 export default function Main() {
   return (
     <>
-      <h1 className="title">
-        Read{' '}
-        <Link href="/posts/first-post">
-          <a>this page!</a>
-        </Link>
-      </h1>
+      <Layout main>
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Header />
+        <h1 className="title">
+          Read{' '}
+          <Link href="/posts/first-post">
+            <a className="seiya">this page!</a>
+          </Link>
+        </h1>
+      </Layout>
     </>
   );
 }

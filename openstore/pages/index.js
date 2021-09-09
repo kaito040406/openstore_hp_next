e60c40fcd7a11179e0c4532af26a97ae2c2a5f84
ookuraseiya <seiya.ookura@gmail.com>
2021-09-09 14:38:40 +0900

    ファイル整理

--- openstore/pages/index.js
+++ openstore/pages/index.js
@@ -1,28 +1,14 @@
-import Head from 'next/head';
 import Link from 'next/link';
-import Layout from '../components/Common-footer';
-import Header from '../components/Header';
-import Button from '@material-ui/core/Button';
 
 export default function Main() {
   return (
     <>
-      <Layout main>
-        <Head>
-          <title>Create Next App</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Header />
-        <h1 className="title">
-          Read{' '}
-          <Link href="/posts/first-post">
-            <a className="seiya">this page!</a>
-          </Link>
-        </h1>
-        <Button variant="contained" color="primary">
-          Test
-        </Button>
-      </Layout>
+      <h1 className="title">
+        Read{' '}
+        <Link href="/posts/first-post">
+          <a>this page!</a>
+        </Link>
+      </h1>
     </>
   );
 }

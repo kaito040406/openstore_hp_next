9ea1147832dd484c0ba377f4ceed7dcefeb307ff
ookuraseiya <seiya.ookura@gmail.com>
2021-09-15 11:24:14 +0900

    Update Top.js

--- openstore/components/Top.js
+++ openstore/components/Top.js
@@ -1,5 +1,27 @@
 import Button from '@material-ui/core/Button';
-import React from 'react';
+import React, { useRef } from 'react';
+import { Canvas, useFrame } from 'react-three-fiber';
+
+const Thing = () => {
+  const ref = useRef();
+  useFrame(() => (ref.current.rotation.z += 0.01));
+  return (
+    <mesh
+      ref={ref}
+      onClick={(e) => console.log('click')}
+      onPointerOver={(e) => console.log('hover')}
+      onPointerOut={(e) => console.log('unhover')}
+    >
+      <planeBufferGeometry attach="geometry" args={[1, 1]} />
+      <meshBasicMaterial
+        attach="material"
+        color="hotpink"
+        opacity={0.5}
+        transparent
+      />
+    </mesh>
+  );
+};
 
 export default function Top() {
   return (

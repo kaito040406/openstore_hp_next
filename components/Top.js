56d9f8d210f1f2e84de4d0db4cffe2be1fb57fe2
ookuraseiya <seiya.ookura@gmail.com>
2021-09-16 11:25:08 +0900

    three.js 導入途中４

--- components/Top.js
+++ components/Top.js
@@ -1,40 +1,73 @@
 import Button from '@material-ui/core/Button';
-import React, { useRef } from 'react';
-// import * as THREE from 'three';
-import * as CANNON from 'cannon';
+import React, { useRef, useMemo } from 'react';
+import * as THREE from 'three';
+// import * as CANNON from 'cannon';
 // import React, { useEffect, useState } from 'react';
-import { Canvas, useFrame } from 'react-three-fiber';
-import { useCannon, Provider } from '../src/useCannon';
+import { Canvas, useLoader } from 'react-three-fiber';
+// import { useCannon, Provider } from '../src/useCannon';
+// import bold from '../resources/bold.blob';
 
 // 無限に落ちるのを防ぐための平面のコード
 
-const Plane = ({ position }) => {
-  const ref = useCannon({ mass: 0 }, (body) => {
-    body.addShape(new CANNON.Plane());
-    body.position.set(...position);
-  });
-  return (
-    <mesh ref={ref} receiveShadow>
-      <planeBufferGeometry attach="geometry" args={[1000, 1000]} />
-      <meshPhongMaterial attach="material" opacity={0} />
-    </mesh>
-  );
-};
+// const Plane = ({ position }) => {
+//   const ref = useCannon({ mass: 0 }, (body) => {
+//     body.addShape(new CANNON.Plane());
+//     body.position.set(...position);
+//   });
+//   return (
+//     <mesh ref={ref} receiveShadow>
+//       <planeBufferGeometry attach="geometry" args={[1000, 1000]} />
+//       <meshPhongMaterial attach="material" opacity={0} />
+//     </mesh>
+//   );
+// };
 
 // 落ちていく物体のコード
 
-const Box = ({ position, args }) => {
-  const ref = useCannon({ mass: 100000 }, (body) => {
-    body.addShape(new CANNON.Box(new CANNON.Vec3(1, 1, 1)));
-    body.position.set(...position);
-  });
+// const Box = ({ position, args }) => {
+//   const ref = useCannon({ mass: 100000 }, (body) => {
+//     body.addShape(new CANNON.Box(new CANNON.Vec3(1, 1, 1)));
+//     body.position.set(...position);
+//   });
+//   return (
+//     <mesh ref={ref} castShadow receiveShadow>
+//       <boxGeometry attach="geometry" args={args} />
+//       <meshStandardMaterial attach="material" />
+//     </mesh>
+//   );
+// };
+
+function Text({}) {
+  // const font = THREE.FontLoader;
+  // const config = useMemo(
+  //   () => ({
+  //     font,
+  //     size: 30,
+  //   }),
+  //   [font]
+  // );
+  // const mesh = useRef([st]);
   return (
-    <mesh ref={ref} castShadow receiveShadow>
-      <boxGeometry attach="geometry" args={args} />
-      <meshStandardMaterial attach="material" />
-    </mesh>
+    <div></div>
+    // <mesh ref={mesh}>
+    //   <textGeometry attach="geometry" args={[st, config]} />
+    //   <meshNormalMaterial attach="material" />
+    // </mesh>
+  );
+}
+
+const BoldText = () => {
+  return (
+    <>
+      <mesh castShadow receiveShadow>
+        <boxGeometry attach="geometry" args={[4, 4, 4]} />
+        <meshStandardMaterial attach="material" />
+        {/* <Text position={[2, 2, 2]} st="3D" size={1.3} /> */}
+      </mesh>
+    </>
   );
 };
+const textGeo = new THREE.TextGeometry ( "Hello, World!", { } );
 
 export default function Top() {
   return (
@@ -66,10 +99,8 @@ export default function Top() {
                 penumbra={1}
                 castShadow
               />
-              <Provider>
-                <Plane position={[0, 0, -10]} />
-                <Box position={[1, 0, 1]} args={[2, 2, 2]} />
-              </Provider>
+              {/* three.js表示 */}
+              <BoldText />
             </Canvas>
           </div>
         </div>
